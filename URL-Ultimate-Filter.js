9ae97c28bff79fda843e17752c4fa44da060cab4
fkhb90 <66230634+fkhb90@users.noreply.github.com>
2025-08-31 16:48:53 +0800

    Update URL-Ultimate-Filter.js

--- URL-Ultimate-Filter.js
+++ URL-Ultimate-Filter.js
@@ -95,7 +95,7 @@ const API_WHITELIST_EXACT = new Set([
     'ebank.megabank.com.tw', 'ibank.firstbank.com.tw', 'netbank.hncb.com.tw', 'mma.sinopac.com',
     'richart.tw', 'api.irentcar.com.tw', 'ebank.tcb-bank.com.tw', 'ibanking.scsb.com.tw',
     'ebank.taipeifubon.com.tw', 'nbe.standardchartered.com.tw', 'usiot.roborock.com', 
-    'a-api.anthropic.com', 'obs.line-scdn.net', 'legy.line-apps.com', 'gemini.google.com', 'obs-tw-line-apps.com'
+    'a-api.anthropic.com', 'obs.line-scdn.net', 'legy.line-apps.com', 'gemini.google.com', 'obs-tw.line-apps.com'
 ]);
 
 const API_WHITELIST_WILDCARDS = new Map([
