1548176c54ca2eecdb33cb0655d27e684edf4fea
fkhb90 <66230634+fkhb90@users.noreply.github.com>
2026-05-12 15:08:57 +0800

    rule: del: acs.m.taobao.com

--- youtube-app-banner.js
+++
@@ -1,7 +0,0 @@
-// remove-youtube-app-banner.js
-if ($response.body) {
-  let body = $response.body.replace(/<meta name="apple-itunes-app"[^>]+>/i, "");
-  $done({ body });
-} else {
-  $done({});
-}
